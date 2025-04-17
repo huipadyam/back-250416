8c4af5799c774dce0a818495b0d0510ad7d62e8d
huipadyam <kces0822@gmail.com>
2025-04-17 12:41:18 +0900

    s3 테스트 통과 안하는 오류 해결, 도커 이미지 푸시 자동화

--- src/test/kotlin/com/example/back/BackApplicationTests.kt
+++ src/test/kotlin/com/example/back/BackApplicationTests.kt
@@ -1,13 +1,39 @@
 package com.example.back
 
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.validation.Validator
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 class BackApplicationTests {
 
+    @Autowired
+    private lateinit var mvc: MockMvc
+
     @Test
-    fun contextLoads() {
+    @DisplayName("GET /buckets")
+    fun t1() {
+        val resultAcions = mvc
+            .perform(
+                get("/buckets")
+            )
+            .andDo(MockMvcResultHandlers.print())
+
+        resultAcions
+            .andExpect {
+                status().isOk()
+            }
     }
 
 }
